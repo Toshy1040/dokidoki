0be03044e9b4cdbcb1cecd5c5107abb85212f7a2
Toshy1040 <104zizi@gmail.com>
2026-07-10 04:21:32 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tokimeku-v21';
+const CACHE = 'tokimeku-v22';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
